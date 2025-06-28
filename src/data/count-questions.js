e8e88af613e92a572b7327625b882631c8aed529
unknown <rayalpj@yahoo.com>
2025-06-28 07:18:55 -0400

    Updates

--- src/data/count-questions.js
+++
@@ -1,9 +0,0 @@
-import { questionBankPSI } from './questions-psi.js';
-
-let total = 0;
-for (const region of Object.values(questionBankPSI)) {
-  for (const section of Object.values(region)) {
-    total += section.length;
-  }
-}
-console.log(`Total questions: ${total}`);
